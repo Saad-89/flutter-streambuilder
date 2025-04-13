1f2c1657974bd633c77e9c29d879044a2fad89fb
saad-yaqoob <saadyaqoob595@gmail.com>
2025-04-14 00:25:36 +0500

    streambuilder real life example

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,17 +1,48 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:stream_builder/tast_model.dart';
+
+class HomePage extends StatefulWidget {
   HomePage({super.key});
 
-  late int i;
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-  Stream<int> count() async* {
-    for (i = 0; i <= 10; i++) {
-      await Future.delayed(Duration(seconds: 1));
-      yield i;
-    }
+class _HomePageState extends State<HomePage> {
+  StreamController<List<Task>> _taskStreamController =
+      StreamController<List<Task>>();
+
+  List<Task> _task = [
+    Task(id: 'task1', taskName: 'Homework Done', isDone: false),
+    Task(id: 'task2', taskName: 'Play football', isDone: false),
+    Task(id: 'task3', taskName: 'Go to gym', isDone: false),
+  ];
+
+  @override
+  void initState() {
+    _taskStreamController.add(_task);
+    super.initState();
   }
 
+  void _addTask(String taskName) {
+    final newTask = Task(
+      id: "task${_task.length}",
+      taskName: taskName,
+      isDone: false,
+    );
+    _task.add(newTask);
+    _taskStreamController.add(_task);
+  }
+
+  @override
+  void dispose() {
+    _taskStreamController.close();
+    super.dispose();
+  }
+
+  TextEditingController _textController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,31 +52,64 @@ class HomePage extends StatelessWidget {
         centerTitle: true,
         title: Text('StreamBuilder', style: TextStyle(color: Colors.white)),
       ),
-      body: Center(
-        child: StreamBuilder(
-          stream: count(),
-          initialData: 10,
-          builder: (context, snapshot) {
-            if (snapshot.hasError) {
-              return Text('Error: ${snapshot.error}');
-            }
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return CircularProgressIndicator(color: Colors.deepPurpleAccent);
-            }
-            if (snapshot.hasData) {
-              return Text(
-                "${snapshot.data}",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          StreamBuilder<List<Task>>(
+            stream: _taskStreamController.stream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.deepPurpleAccent,
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(child: Text('Error: ${snapshot.error}'));
+              } else if (!snapshot.hasData) {
+                return Center(child: Text('No data'));
+              } else {
+                final tasks = snapshot.data;
+                return Expanded(
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: tasks!.length,
+                    itemBuilder: (context, index) {
+                      final task = tasks[index];
+                      return ListTile(
+                        leading: Text('${task.taskName}'),
+                        trailing:
+                            task.isDone
+                                ? Icon(Icons.check)
+                                : Icon(Icons.check_box_outline_blank),
+                      );
+                    },
+                  ),
+                );
+              }
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.all(9),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(border: OutlineInputBorder()),
                 ),
-              );
-            } else {
-              return Text('Done counting!');
-            }
-          },
-        ),
+                SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_textController.text.isNotEmpty) {
+                      _addTask(_textController.text.toString());
+                      _textController.clear();
+                    }
+                  },
+                  child: Text('Add'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/tast_model.dart
@@ -0,0 +1,7 @@
+class Task {
+  final String id;
+  final String taskName;
+  final bool isDone;
+
+  Task({required this.id, required this.taskName, required this.isDone});
+}
